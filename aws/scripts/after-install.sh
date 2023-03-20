1e0b288c1fec94f87ce275e4ff761328b23058f5
Biswash Giri <biswash.giri@adex.ltd>
2023-03-20 19:26:08 +0545

    added revision dynamically

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -5,9 +5,12 @@ set -xe
 # Copy war file from S3 bucket to tomcat webapp folder
 # aws s3 cp s3://tomcatjavafortest/SpringBootHelloWorldExampleApplication.war /var/lib/tomcat9/webapps/ROOT.war
 
-aws s3 cp s3://tomcatjavafortest/myapp-${{ github.sha }}.zip /tmp/application
-unzip -qq ./myapp-${{ github.sha }}.zip -d .
-cp *.war /var/lib/tomcat9/webapps/ROOT.war
+# aws s3 cp s3://tomcatjavafortest/myapp-${{ github.sha }}.zip /tmp/application
+# unzip -qq ./myapp-${{ github.sha }}.zip -d .
+# cp *.war /var/lib/tomcat9/webapps/ROOT.war
+
+
+mv /usr/local/codedeployresources/*.war /var/lib/tomcat9/webapps/ROOT.war
 
 
 # Ensure the ownership permissions are correct.
