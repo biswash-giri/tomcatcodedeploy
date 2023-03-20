d984f82989334a02b0aa561a5e20c60e3e3e4b8b
Biswash Giri <biswash.giri@adex.ltd>
2023-03-20 13:48:43 +0545

    changed script file

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,8 +3,8 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://##s3-bucket##/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://tomcatjavafortest/app.war /var/lib/tomcat9/webapps/app.war
 
 
 # Ensure the ownership permissions are correct.
-chown -R tomcat:tomcat /usr/local/tomcat9/webapps
+chown -R tomcat:tomcat /var/lib/tomcat9/webapps
