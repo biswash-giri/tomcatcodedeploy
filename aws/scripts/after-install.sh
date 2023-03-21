43c91e19667ffc5834e8269a19d2e4c126d49b7e
Biswash Giri <biswash.giri@adex.ltd>
2023-03-21 07:52:51 +0545

    changed pipeline

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -4,7 +4,7 @@ set -xe
 
 # Copy war file from S3 bucket to tomcat webapp folder
 
-mv /usr/local/codedeployresources/aws/*.war /var/lib/tomcat9/webapps/ROOT.war
+mv /usr/local/codedeployresources/*.war /var/lib/tomcat9/webapps/ROOT.war
 
 
 # Ensure the ownership permissions are correct.
